65f7de1516dcaf3000ae97ad68ddcff1dd2aeb21
hsfk <hsfk@yandex.ru>
2017-03-08 03:04:17 +1000

    err: add scc_err_t type

--- scc/err.h
+++ scc/err.h
@@ -1,6 +1,11 @@
 #ifndef SCC_ERR_H
 #define SCC_ERR_H
 
+#define SCC_SUCCESS 0
+#define SCC_ERR     1
+
+typedef int scc_err_t;
+
 extern void scc_error(const char* msg);
 extern void scc_warning(const char* msg);
 extern const char* scc_last_error();
