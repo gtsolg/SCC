edbab0f470eac03b523cd16c9c0556b7b554a6c4
hsfk <hsfk@yandex.ru>
2017-03-08 03:07:30 +1000

    csymtab: add globl symtab

--- scc/c/csymtab.c
+++ scc/c/csymtab.c
@@ -1,6 +1,15 @@
 #include "c/csymtab.h"
 #include "hash.h"
-#include "c/clexer.h"
+
+extern void c_symtab_init(struct c_symtab* symtab, struct htab* globl)
+{
+        symtab->globl = globl;
+        symtab->scope_idx = 0;
+
+        struct htab empty = htab_init(10);
+        for (size_t i = 0; i < C_MAX_BLOCK_NESTING; i++)
+                symtab->scope[i] = empty;
+}
 
 extern struct c_symtab* c_symtab_create()
 {
@@ -32,10 +41,22 @@ extern void c_symtab_exit_scope(struct c_symtab* symtab)
 
 extern void c_symtab_add_symb(struct c_symtab* symtab, tree symb, uint64_t key)
 {
-        htab_insert(&symtab->scope[symtab->scope_idx], key, symb);
+        htab_insert(symtab->scope + symtab->scope_idx, key, symb);
+}
+
+extern void c_symtab_add_globl_symb(struct c_symtab* symtab, tree symb, uint64_t key)
+{
+        htab_insert(symtab->globl, key, symb);
 }
 
 extern tree c_symtab_get_symb(struct c_symtab* symtab, uint64_t key)
 {
-        return htab_get(&symtab->scope[symtab->scope_idx], key);
+        tree symb = NULL;
+        for (size_t i = symtab->scope_idx; i != -1; i--)
+        {
+                symb = htab_get(symtab->scope + i, key);
+                if (symb)
+                        return symb;
+        }
+        return htab_get(symtab->globl, key);
 }
--- scc/c/csymtab.h
+++ scc/c/csymtab.h
@@ -7,16 +7,21 @@
 
 struct c_symtab
 {
-        struct htab scope[C_MAX_BLOCK_NESTING];
+        struct htab* globl;
         size_t scope_idx;
+        struct htab scope[C_MAX_BLOCK_NESTING];
 };
 
+extern void c_symtab_init(struct c_symtab* symtab, struct htab* globl);
+
 extern struct c_symtab* c_symtab_create();
+
 extern void c_symtab_delete(struct c_symtab* symtab);
 extern void c_symtab_enter_scope(struct c_symtab* symtab);
 extern void c_symtab_exit_scope(struct c_symtab* symtab);
             
 extern void c_symtab_add_symb(struct c_symtab* symtab, tree symb, uint64_t key);
+extern void c_symtab_add_globl_symb(struct c_symtab* symtab, tree symb, uint64_t key);
 extern tree c_symtab_get_symb(struct c_symtab* symtab, uint64_t key);
 
 #endif // !C_SYMTAB_H
