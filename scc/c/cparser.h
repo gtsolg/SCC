2361ec5285e81f82be7cdb5ab56d57c794b0ff53
hsfk <hsfk@yandex.ru>
2017-03-11 19:08:09 +1000

    move tree related function to ctree

--- scc/c/cparser.h
+++ scc/c/cparser.h
@@ -2,11 +2,11 @@
 #define C_PARSER_H
 
 #include "cparser_internal.h"
+#include "ctree.h"
 
 extern tree c_parse_base_type(struct c_parser* parser, struct c_symtab* symtab);
 extern tree c_parse_type(struct c_parser* parser, struct c_symtab* symtab, size_t size);
 
-extern int c_node_is_operand(tree node);
 extern tree c_parse_expr_raw(struct c_parser* parser, struct c_symtab* symtab, size_t size);
 extern tree c_parse_expr(struct c_parser* parser, struct c_symtab* symtab, size_t size);
 
