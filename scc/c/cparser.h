315563d680cd59fc8e5eb62d66923677ec1246e1
hsfk <hsfk@yandex.ru>
2017-03-25 23:11:43 +1000

    cparser: add type iterator, c_type_parse_info

--- scc/c/cparser.h
+++ scc/c/cparser.h
@@ -4,8 +4,23 @@
 #include "cparser_internal.h"
 #include "ctree.h"
 
-extern tree c_parse_base_type(struct c_parser* parser, struct c_symtab* symtab);
-extern tree c_parse_type(struct c_parser* parser, struct c_symtab* symtab, size_t size);
+struct c_type_parse_info
+{
+        enum decl_storage_class storage_class;
+        enum func_call_type call_type;
+        enum type_qualifier qual;
+
+        unsigned past_base : 1;
+        unsigned is_right_subtree : 1;
+};
+
+#define c_type_parse_info_init() { dsc_auto, fct_auto, tq_unqualified, 0 }
+#define c_type_iterator_info(ptree) ((struct c_type_parse_info*)tree_iterator_data(ptree))
+
+extern scc_err_t c_type_iterator_initf(tree it, tree exp, struct c_type_parse_info* info);
+extern tree      c_type_iterator_advance(tree it);
+extern tree      c_parse_base_type(struct c_parser* parser, struct c_symtab* symtab);
+extern tree      c_parse_type(struct c_parser* parser, struct c_symtab* symtab, size_t size);
 
 extern tree c_parse_expr_raw(struct c_parser* parser, struct c_symtab* symtab, size_t size);
 extern tree c_parse_expr(struct c_parser* parser, struct c_symtab* symtab, size_t size);
