938987bed17092f31895d748120478fb96f6a5e3
hsfk <hsfk@yandex.ru>
2017-04-08 12:00:03 +1000

    tree_pass: remove deprecated code

--- scc/tree_pass.c
+++ scc/tree_pass.c
@@ -2,10 +2,4 @@
 
 extern void tree_walk_list(tree list, tree_pass_fn pass, void* info)
 {
-        tree it = tree_list_iterator_init(list);
-        while (tree_list_iterator_valid(it))
-        {
-                pass(tree_list_node_base(it), info);
-                tree_list_iterator_advance(it);
-        }
 }
