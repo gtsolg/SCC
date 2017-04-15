8253b6cc9dce2357fb21cf14d3f42b6f56e0e8f4
hsfk <hsfk@yandex.ru>
2017-04-15 13:19:39 +1000

    ctree: fix indents

--- scc/c/tree/ctree.h
+++ scc/c/tree/ctree.h
@@ -3,14 +3,15 @@
 
 #include "../ccommon.h"
 
-extern enum type_qualifier ctt_to_qualifier(enum c_token_type type);
-extern enum func_call_type ctt_to_call_type(enum c_token_type type);
+extern enum type_qualifier     ctt_to_qualifier(enum c_token_type type);
+extern enum func_call_type     ctt_to_call_type(enum c_token_type type);
 extern enum decl_storage_class ctt_to_storage_class(enum c_token_type type);
 
-extern enum type_qualifier c_token_to_qualifier(const c_token* token);
+extern enum type_qualifier     c_token_to_qualifier(const c_token* token);
 extern enum decl_storage_class c_token_to_storage_class(const c_token* token);
 
 extern int c_node_is_exp(tree node);
+extern int c_node_is_assign(tree node);
 extern int c_node_is_operand(tree node);
 extern int c_node_is_postfix_operator(tree node);
 extern int c_list_node_is_exp(tree node, enum expr_node_kind op);
